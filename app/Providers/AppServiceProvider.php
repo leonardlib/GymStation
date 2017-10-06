47049298ab53ae50ba80a335fb3126e36db39fd6
Leonardo Lira Becerra <leonardolirabecerra@MacBook-de-Leonardo.local>
2017-10-05 21:19:35 -0500

    Commit inicial

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot() {
+        //Asignar un tamaño para los atributos de string en la base de datos
+        //corrigiendo la excepción de desborde de tamaño
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
